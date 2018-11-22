c841f48347a2787465a02c861940c1d956d868c4
Levi Balling <leviballing@gmail.com>
2018-11-22 08:53:42 -0700

    Added unit test to validate that the temperature increases

--- tests/test_hvac_building.py
+++ tests/test_hvac_building.py
@@ -30,3 +30,23 @@ def test_HVAC_off(hvacBuilding: HvacBuilding):
 		hvacBuilding.step(0, 30, 25)
 		assert hvacBuilding.current_temperature < currentBuildingTemperature
 		currentBuildingTemperature = hvacBuilding.current_temperature
+		
+def test_HVAC_on(hvacBuilding: HvacBuilding):
+	"""Tests the typical building gets colder and colder when the heater and cooler are both off
+	
+	Arguments:
+		hvacBuilding {HvacBuilding} -- the hvac Building test fixture object
+	"""
+	# check that the temperature is getting cooler every time
+	currentBuildingTemperature = 22
+	# turn on the furnace, it shouldn't change for the first while till we have completed the startup
+	# so it will say at 22 for the first section
+	hvacBuilding.building_hvac.TurnHeatingOn()
+
+	for i in range(60):
+		hvacBuilding.step(55, 0, 0)
+		hvacBuilding.building_hvac.SimulateOneSecond()
+		assert hvacBuilding.current_temperature > currentBuildingTemperature
+		currentBuildingTemperature = hvacBuilding.current_temperature
+	
+	assert hvacBuilding.building_hvac.TotalPowerUsed > 0
