7ba0e9e7101ab0b1b94d6c317e2a0a8e718e40a0
Levi Balling <leviballing@gmail.com>
2018-11-26 10:58:41 -0700

    Renamed the file

--- hvac_building_example.py
+++ hvac_building_example.py
@@ -23,25 +23,40 @@ hvacBuilding = HvacBuilding(
 # a set of temperatures in Northern Utah, USA for one day
 loganOutsideTemperatures = [1.11, 2.22, 1.67, 1.67, 2.22, 1.11, 1.11, 2.78, 4.44, 4.44, 5.56, 6.67, 6.67, 7.22, 6.67, 2.22, 2.22, 1.67, 1.11, 1.11, 0.56, 1.11, 0.00, 0.00]
 
+print()
+print("Starting Hvac Building Example")
+print()
 
 # simulate one day
+numberOfHeatingOn = 0
 for outsideTemperature in loganOutsideTemperatures:
 	# iterate through one hour with the same temperature
 	for	i in range(3600):
 		hvacBuilding.step(outsideTemperature)
-		if hvacBuilding.current_temperature > 21:
+		if not hvac.HeatingIsShuttingDown and hvac.HeatingIsOn and hvacBuilding.current_temperature > 21:
+			print("Turning the Heater Off")
 			hvac.TurnHeatingOff()
-		if hvacBuilding.current_temperature < 17:
+
+		if hvac.HeatingIsOn == False and hvacBuilding.current_temperature < 17:
+			print("Turning the Heater On")
+			numberOfHeatingOn = numberOfHeatingOn + 1
 			hvac.TurnHeatingOn()
 print()
+print("     RESULTS    ")
 print()
-print()
-print()
+print("The Number of times the furnace turns on: " + str(numberOfHeatingOn))
 print("The Current Temperature: " + str(hvacBuilding.current_temperature) + "C")
 print("The total power used: " + str(hvac.TotalPowerUsed) + "Watts")
 print("The total power used: " + str(hvac.TotalPowerUsed / 1000) + "KWatts")
 print("The total Time furnace is on: " + str(hvac.TotalTimeInSeconds))
-print("The Average Watts Per Second: " + str(hvac.GetAverageWattsPerSecond()))
+wattsPerSecond = hvac.GetAverageWattsPerSecond()
+print("The Average Watts Per Second: " + str(wattsPerSecond))
+
+# convert to Kilowatts per second
+KilowattHour = wattsPerSecond /1000
+# convert to kilowatts per hour
+KilowattHour = KilowattHour * 3600
+print("The Average KWH: " + str(KilowattHour))
 totalEnergy = hvac.TotalPowerUsed
 totalGasEnergyUsed = hvac.GetTotalGasEnergyUsed()
 
